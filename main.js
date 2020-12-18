2bf22116992d024a5485a0975e5aae251db22b63
DominicWrege <dominic.wrege001@stud.fh-dortmund.de>
2020-12-18 19:40:58 +0100

    more refactor

--- main.js
+++ main.js
@@ -1,66 +1,8 @@
-async function downloadAnswer(linkElement) {
-    const link = linkElement.getAttribute("data-answer-href");
-    let response = await fetch(
-        `${this.window.location.origin}/ilias/${link}`
-    );
-    if (!response.ok) {
-        throw new Error("Error while download the answer")
-    }
-    return response.text();
-}
-
-function parseAnswer(text) {
-    const html = new DOMParser().parseFromString(text, "text/html");
-    return {
-        html: html,
-        anwserText: html.querySelector("div.ilc_qanswer_Answer").textContent
-    }
-}
-
-function createAnswerDiv(textContent, widthStyle) {
-    const div = document.createElement("div");
-    div.style.width = widthStyle;
-    div.style.maxWidth = "460px";
-    div.textContent = textContent
-    return div;
-}
-
-let questionIntoTitleInserted = false;
-function insertQuestionIntoTitle(html) {
-    const questionElement = html.querySelector(
-        "div.ilc_qtitle_Title"
-    );
-    questionElement.style =
-        "background-color: #fff;padding: 0.4em;margin: 0.5em 0 0.5em 0;";
-    const titleH3 = document.querySelector("h3.ilTableHeaderTitle");
-    titleH3.parentElement.appendChild(questionElement);
-    questionIntoTitleInserted = true;
-
-}
-
-async function replaceAnswerShowQuestion(linkElement, widthStyle) {
-    if (linkElement) {
-        const parsedResponse = parseAnswer(await downloadAnswer(linkElement));
-        if (parsedResponse.anwserText != null && parsedResponse.anwserText != undefined) {
-            linkElement.parentElement.replaceChild(
-                createAnswerDiv(parsedResponse.anwserText, widthStyle),
-                linkElement
-            );
-        } else {
-            throw "Ilias error :/";
-        }
-        if (!questionIntoTitleInserted) {
-            insertQuestionIntoTitle(parsedResponse.html);
-        }
-    }
-}
-
-
 /*
 
    Explanation:
 
-    1. Get all the a elmenets who are just links to answers from the table
+    1. Get all the a elmenets who are just links to the answers from the table
     2. Extract the url from the a element 
     3. Fetch the annswer
     4. Parse the html from the response
@@ -100,7 +42,7 @@ function fixAnswersMain(e) {
 
 const bar = document.querySelector(".ilTableCommandRowTop > div:nth-child(2)");
 const tabActive = document.querySelector("#tab_manscoring.active");
-
+let questionIntoTitleInserted = false;
 if (bar && tabActive) {
     const btn = document.createElement("button");
     btn.classList = ["btn", "btn-default"];
@@ -115,4 +57,61 @@ if (bar && tabActive) {
     loading.textContent = "loading...";
     bar.prepend(loading);
     bar.prepend(btn);
+}
+
+
+async function downloadAnswer(linkElement) {
+    const link = linkElement.getAttribute("data-answer-href");
+    let response = await fetch(
+        `${this.window.location.origin}/ilias/${link}`
+    );
+    if (!response.ok) {
+        throw new Error("Error while download the answer")
+    }
+    return response.text();
+}
+
+function parseAnswer(text) {
+    const html = new DOMParser().parseFromString(text, "text/html");
+    return {
+        html: html,
+        anwserText: html.querySelector("div.ilc_qanswer_Answer").textContent
+    }
+}
+
+function createAnswerDiv(textContent, widthStyle) {
+    const div = document.createElement("div");
+    div.style.width = widthStyle;
+    div.style.maxWidth = "460px";
+    div.textContent = textContent
+    return div;
+}
+
+function insertQuestionIntoTitle(html) {
+    const questionElement = html.querySelector(
+        "div.ilc_qtitle_Title"
+    );
+    questionElement.style =
+        "background-color: #fff;padding: 0.4em;margin: 0.5em 0 0.5em 0;";
+    const titleH3 = document.querySelector("h3.ilTableHeaderTitle");
+    titleH3.parentElement.appendChild(questionElement);
+    questionIntoTitleInserted = true;
+
+}
+
+async function replaceAnswerShowQuestion(linkElement, widthStyle) {
+    if (linkElement) {
+        const parsedResponse = parseAnswer(await downloadAnswer(linkElement));
+        if (parsedResponse.anwserText != null && parsedResponse.anwserText != undefined) {
+            linkElement.parentElement.replaceChild(
+                createAnswerDiv(parsedResponse.anwserText, widthStyle),
+                linkElement
+            );
+        } else {
+            throw "Ilias error :/";
+        }
+        if (!questionIntoTitleInserted) {
+            insertQuestionIntoTitle(parsedResponse.html);
+        }
+    }
 }
