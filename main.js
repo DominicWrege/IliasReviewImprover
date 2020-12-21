655b0f6fea38d27a56c0311c7d288960360f0c2f
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-12-21 17:39:30 +0100

    font switcher and only append answer

--- main.js
+++ main.js
@@ -10,8 +10,10 @@
     5. Do it only once: Insert the question into the title
 
 */
+let questionIntoTitleInserted = false;
+main();
 
-function fixAnswersMain(e) {
+function fixAnswers(e) {
     e.preventDefault();
     e.stopPropagation();
 
@@ -32,6 +34,7 @@ function fixAnswersMain(e) {
             showAllAnswersBtn.hidden = true;
             Promise.all(promises)
                 .then(() => {
+                    loadingDiv.parentElement.prepend(checkBoxFont());
                     loadingDiv.remove();
                     showAllAnswersBtn.remove();
                 })
@@ -40,33 +43,54 @@ function fixAnswersMain(e) {
     });
 }
 
-const bar = document.querySelector(".ilTableCommandRowTop > div:nth-child(2)");
-const tabActive = document.querySelector("#tab_manscoring.active");
-let questionIntoTitleInserted = false;
-if (bar && tabActive) {
+function main() {
+    const bar = document.querySelector(
+        ".ilTableCommandRowTop > div:nth-child(2)"
+    );
+    const tabActive = document.querySelector("#tab_manscoring.active");
+    if (bar && tabActive) {
+        bar.prepend(fixButton());
+        bar.prepend(loadingText());
+    }
+}
+
+function loadingText() {
+    const loading = document.createElement("div");
+    loading.style = "margin-right: 5em;display:none";
+    loading.id = "loadingText";
+    loading.textContent = "loading...";
+    return loading;
+}
+
+function fixButton() {
     const btn = document.createElement("button");
     btn.classList = ["btn", "btn-default"];
     btn.id = "ImproveReview";
     btn.textContent = "Improve Review";
     btn.style.marginRight = "16px";
-    btn.addEventListener("click", fixAnswersMain);
-    const loading = document.createElement("div");
-
-    loading.style = "margin-right: 5em;display:none";
-    loading.id = "loadingText";
-    loading.textContent = "loading...";
-    bar.prepend(loading);
-    bar.prepend(btn);
+    btn.addEventListener("click", fixAnswers);
+    return btn;
 }
 
+function toogleFont(event) {
+    const checkbox = event.target;
+    const answers = document.querySelectorAll(
+        "td.std div div.ilc_qanswer_Answer.solutionbox"
+    );
+
+    for (const element of answers)
+        if (checkbox.checked) {
+            element.style.fontFamily = `Courier, Monaco, monospace`;
+        } else {
+            element.style.fontFamily = "";
+        }
+}
 
 async function downloadAnswer(linkElement) {
     const link = linkElement.getAttribute("data-answer-href");
-    let response = await fetch(
-        `${this.window.location.origin}/ilias/${link}`
-    );
+    let response = await fetch(`${this.window.location.origin}/ilias/${link}`);
     if (!response.ok) {
-        throw new Error("Error while download the answer")
+        throw new Error("Error while download the answer");
     }
     return response.text();
 }
@@ -75,36 +99,38 @@ function parseAnswer(text) {
     const html = new DOMParser().parseFromString(text, "text/html");
     return {
         html: html,
-        anwserText: html.querySelector("div.ilc_qanswer_Answer").textContent
-    }
+        anwser: html.querySelector("div.ilc_qanswer_Answer"),
+    };
 }
 
-function createAnswerDiv(textContent, widthStyle) {
+function createAnswerDiv(anwser, widthStyle) {
     const div = document.createElement("div");
     div.style.width = widthStyle;
-    div.style.maxWidth = "460px";
-    div.textContent = textContent
+    div.style.padding = 0;
+    div.style.background = "#FFF !important";
+    anwser.style.border = "none";
+    div.style.maxWidth = "500px";
+    anwser.style.color = "black";
+    div.appendChild(anwser);
     return div;
 }
 
 function insertQuestionIntoTitle(html) {
-    const questionElement = html.querySelector(
-        "div.ilc_qtitle_Title"
-    );
+    const questionElement = html.querySelector("div.ilc_qtitle_Title");
     questionElement.style =
         "background-color: #fff;padding: 0.4em;margin: 0.5em 0 0.5em 0;";
     const titleH3 = document.querySelector("h3.ilTableHeaderTitle");
     titleH3.parentElement.appendChild(questionElement);
     questionIntoTitleInserted = true;
-
 }
 
 async function replaceAnswerShowQuestion(linkElement, widthStyle) {
     if (linkElement) {
         const parsedResponse = parseAnswer(await downloadAnswer(linkElement));
-        if (parsedResponse.anwserText != null && parsedResponse.anwserText != undefined) {
+        if (parsedResponse.anwser) {
             linkElement.parentElement.replaceChild(
-                createAnswerDiv(parsedResponse.anwserText, widthStyle),
+                createAnswerDiv(parsedResponse.anwser, widthStyle),
+                //parsedResponse.anwser,
                 linkElement
             );
         } else {
@@ -114,4 +140,22 @@ async function replaceAnswerShowQuestion(linkElement, widthStyle) {
             insertQuestionIntoTitle(parsedResponse.html);
         }
     }
+}
+
+function checkBoxFont() {
+    const wrapper = document.createElement("div");
+    wrapper.style.display = "inline";
+    wrapper.style.marginRight = "2em";
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.id = "font-style";
+    input.name = "font";
+    input.addEventListener("click", toogleFont);
+    const label = document.createElement("label");
+    label.for = "font-sytle";
+    label.style.paddingLeft = "1em";
+    label.textContent = "Monospace";
+    wrapper.appendChild(input);
+    wrapper.appendChild(label);
+    return wrapper;
 }
