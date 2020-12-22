ee4c925154a6298a1ff9e814bf79f10ca1c5a28f
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-12-22 13:19:30 +0100

    refactor

--- common.js
+++
@@ -1,7 +0,0 @@
-async function checkSettings() {
-    const obj = await browser.storage.local.get("width");
-    if (!obj["width"]) {
-        return 37;
-    }
-    return obj.width;
-}
--- settings.js
+++ settings.js
@@ -1,18 +1,29 @@
 const slider = document.querySelector("#width-slider");
 const valueLabel = document.querySelector("#value-slider");
 
-const unit = "rem";
-
-checkSettings().then((width) => {
-    slider.value = width;
-    valueLabel.textContent = `${width}${unit}`;
-});
-
-slider.addEventListener("input", (e) => {
-    e.preventDefault();
-    const v = e.target.value;
-    valueLabel.textContent = `${v}${unit}`;
-    browser.storage.local.set({
-        width: v,
+if (slider && valueLabel) {
+    const unit = "rem";
+    checkSettings().then((width) => {
+        slider.value = width;
+        valueLabel.textContent = `${width}${unit}`;
     });
-});
+
+    slider.addEventListener("input", (e) => {
+        e.preventDefault();
+        const v = e.target.value;
+        valueLabel.textContent = `${v}${unit}`;
+        browser.storage.local.set({
+            width: v,
+        });
+    });
+}
+
+async function checkSettings() {
+    if (typeof browser !== "undefined" && browser != null) {
+        const obj = await browser.storage.local.get("width");
+        if (obj["width"]) {
+            return obj.width;
+        }
+    }
+    return 38;
+}
