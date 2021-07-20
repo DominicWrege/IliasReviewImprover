76eaa2a4023bbed7899846f3f4bdf8b7a7eab25e
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2021-07-20 22:29:08 +0200

    rewrite settings

--- src/main.js
+++ src/main.js
@@ -14,132 +14,132 @@ let questionIntoTitleInserted = false;
 main();
 
 async function handlerFixAnswers(event) {
-    event.preventDefault();
-    event.stopPropagation();
+	event.preventDefault();
+	event.stopPropagation();
 
-    const loadingDiv = document.querySelector("div#loadingText");
-    loadingDiv.style.display = "inline-block";
-    document.querySelector("input#ImproveReview").remove();
-    // settings.js
-    await fixAllAnswers(`${await checkSettings()}rem`);
+	const loadingDiv = document.querySelector("div#loadingText");
+	loadingDiv.style.display = "inline-block";
+	document.querySelector("input#ImproveReview").remove();
+	// settings.js
+	await fixAllAnswers(`${await loadSettings()}rem`);
 }
 
 async function fixAllAnswers(widthStyle) {
-    let links = document.querySelectorAll(
-        "td.std > a.il_ContainerItemCommand[data-answer-href]"
-    );
-    let promises = [];
-    for (const a of links) {
-        promises.push(replaceAnswerShowQuestion(a, widthStyle));
-    }
+	let links = document.querySelectorAll(
+		"td.std > a.il_ContainerItemCommand[data-answer-href]"
+	);
+	let promises = [];
+	for (const a of links) {
+		promises.push(replaceAnswerShowQuestion(a, widthStyle));
+	}
 
-    if (promises.length > 0) {
-        try {
-            await Promise.all(promises);
-        } catch (err) {
-            console.error("error:", err);
-        }
-        const loadingDiv = document.querySelector("div#loadingText");
-        loadingDiv.parentElement.prepend(checkBoxFont());
-        loadingDiv.remove();
-    }
+	if (promises.length > 0) {
+		try {
+			await Promise.all(promises);
+		} catch (err) {
+			console.error("error:", err);
+		}
+		const loadingDiv = document.querySelector("div#loadingText");
+		loadingDiv.parentElement.prepend(checkBoxFont());
+		loadingDiv.remove();
+	}
 }
 
 function loadingText() {
-    const loading = document.createElement("div");
-    loading.style = "margin-right: 5em;display:none";
-    loading.id = "loadingText";
-    loading.textContent = "loading...";
-    return loading;
+	const loading = document.createElement("div");
+	loading.style = "margin-right: 5em;display:none";
+	loading.id = "loadingText";
+	loading.textContent = "loading...";
+	return loading;
 }
 
 function fixButton() {
-    // util.js
-    const button = createBlueButton("Show Answers");
-    button.id = "ImproveReview";
-    button.addEventListener("click", handlerFixAnswers);
-    return button;
+	// util.js
+	const button = createBlueButton("Show Answers");
+	button.id = "ImproveReview";
+	button.addEventListener("click", handlerFixAnswers);
+	return button;
 }
 
 function toggleFont(event) {
-    const checkbox = event.target;
-    const answers = document.querySelectorAll(
-        "td.std div div.ilc_qanswer_Answer.solutionbox"
-    );
+	const checkbox = event.target;
+	const answers = document.querySelectorAll(
+		"td.std div div.ilc_qanswer_Answer.solutionbox"
+	);
 
-    for (const element of answers)
-        if (checkbox.checked) {
-            element.style.fontFamily = `Courier, Monaco, monospace`;
-        } else {
-            element.style.fontFamily = "";
-        }
+	for (const element of answers)
+		if (checkbox.checked) {
+			element.style.fontFamily = `Courier, Monaco, monospace`;
+		} else {
+			element.style.fontFamily = "";
+		}
 }
 
 function createAnswerDiv(answer, widthStyle) {
-    const div = document.createElement("div");
-    div.style.width = widthStyle;
-    div.style.padding = 0;
-    div.style.background = "#FFF !important";
-    answer.style.border = "none";
-    // div.style.maxWidth = "700px !important";
-    answer.style.color = "black";
-    div.appendChild(answer);
-    return div;
+	const div = document.createElement("div");
+	div.style.width = widthStyle;
+	div.style.padding = 0;
+	div.style.background = "#FFF !important";
+	answer.style.border = "none";
+	// div.style.maxWidth = "700px !important";
+	answer.style.color = "black";
+	div.appendChild(answer);
+	return div;
 }
 
 function insertQuestionIntoTitle(html) {
-    const questionElement = html.querySelector("div.ilc_qtitle_Title");
-    questionElement.style =
-        "background-color: #fff;padding: 0.4em;margin: 0.5em 0 0.5em 0;";
-    const titleH3 = document.querySelector("h3.ilTableHeaderTitle");
-    titleH3.parentElement.appendChild(questionElement);
-    questionIntoTitleInserted = true;
+	const questionElement = html.querySelector("div.ilc_qtitle_Title");
+	questionElement.style =
+		"background-color: #fff;padding: 0.4em;margin: 0.5em 0 0.5em 0;";
+	const titleH3 = document.querySelector("h3.ilTableHeaderTitle");
+	titleH3.parentElement.appendChild(questionElement);
+	questionIntoTitleInserted = true;
 }
 
 async function replaceAnswerShowQuestion(linkElement, widthStyle) {
-    if (linkElement) {
-        // util.js
-        const parsedResponse = parseAnswer(await downloadAnswer(linkElement));
-        if (parsedResponse?.answer) {
-            linkElement.parentElement.replaceChild(
-                createAnswerDiv(parsedResponse.answer, widthStyle),
-                linkElement
-            );
-        } else {
-            throw "Ilias error :/";
-        }
-        if (!questionIntoTitleInserted) {
-            insertQuestionIntoTitle(parsedResponse.html);
-        }
-    }
+	if (linkElement) {
+		// util.js
+		const parsedResponse = parseAnswer(await downloadAnswer(linkElement));
+		if (parsedResponse?.answer) {
+			linkElement.parentElement.replaceChild(
+				createAnswerDiv(parsedResponse.answer, widthStyle),
+				linkElement
+			);
+		} else {
+			throw "Ilias error :/";
+		}
+		if (!questionIntoTitleInserted) {
+			insertQuestionIntoTitle(parsedResponse.html);
+		}
+	}
 }
 
 function checkBoxFont() {
-    const wrapper = document.createElement("div");
-    wrapper.style.display = "inline";
-    wrapper.style.marginRight = "2em";
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    input.id = "font-style";
-    input.name = "font";
-    input.addEventListener("click", toggleFont);
-    const label = document.createElement("label");
-    label.for = "font-sytle";
-    label.style.paddingLeft = "1em";
-    label.textContent = "Monospace";
-    wrapper.appendChild(input);
-    wrapper.appendChild(label);
-    return wrapper;
+	const wrapper = document.createElement("div");
+	wrapper.style.display = "inline";
+	wrapper.style.marginRight = "2em";
+	const input = document.createElement("input");
+	input.type = "checkbox";
+	input.id = "font-style";
+	input.name = "font";
+	input.addEventListener("click", toggleFont);
+	const label = document.createElement("label");
+	label.for = "font-sytle";
+	label.style.paddingLeft = "1em";
+	label.textContent = "Monospace";
+	wrapper.appendChild(input);
+	wrapper.appendChild(label);
+	return wrapper;
 }
 function main() {
-    const bar = document.querySelector(
-        ".ilTableCommandRowTop > div:nth-child(2)"
-    );
-    const tabActive = document.querySelector("#tab_manscoring.active");
-    const mainTable = document.querySelector("tbody>tr>td.ilCenter");
-    if (bar && tabActive && !mainTable) {
-        setupExportButtons();
-        bar.prepend(fixButton());
-        bar.prepend(loadingText());
-    }
+	const bar = document.querySelector(
+		".ilTableCommandRowTop > div:nth-child(2)"
+	);
+	const tabActive = document.querySelector("#tab_manscoring.active");
+	const mainTable = document.querySelector("tbody>tr>td.ilCenter");
+	if (bar && tabActive && !mainTable) {
+		setupExportButtons();
+		bar.prepend(fixButton());
+		bar.prepend(loadingText());
+	}
 }
--- src/settings.js
+++ src/settings.js
@@ -1,29 +1,54 @@
 const slider = document.querySelector("#width-slider");
 const valueLabel = document.querySelector("#value-slider");
+const widthKey = "width";
 
 if (slider && valueLabel) {
-    const unit = "rem";
-    checkSettings().then((width) => {
-        slider.value = width;
-        valueLabel.textContent = `${width}${unit}`;
-    });
+	const unit = "rem";
+	loadSettings().then((width) => {
+		slider.value = width;
+		valueLabel.textContent = `${width}${unit}`;
+	});
 
-    slider.addEventListener("input", (e) => {
-        e.preventDefault();
-        const v = e.target.value;
-        valueLabel.textContent = `${v}${unit}`;
-        browser.storage.local.set({
-            width: v,
-        });
-    });
+	slider.addEventListener("input", (e) => {
+		e.preventDefault();
+		const width = e.target.value;
+		console.log(width);
+		valueLabel.textContent = `${width}${unit}`;
+		saveWidthSettings(width);
+	});
 }
 
-async function checkSettings() {
-    if (typeof browser !== "undefined" && browser !== null) {
-        const obj = await browser.storage.local.get("width");
-        if (obj["width"]) {
-            return obj.width;
-        }
-    }
-    return 40;
+function saveWidthSettings(width) {
+	if (!width) {
+		return;
+	}
+	if (isChrome) {
+		chrome.storage.local.set({ width });
+	} else {
+		browser.storage.local.set({ width: width });
+	}
+}
+
+function isChrome() {
+	return chrome?.storage !== undefined && chrome?.storage !== null;
+}
+
+async function loadSettings() {
+	const defaultWidth = 40;
+	if (!isChrome) {
+		const obj = await browser.storage.local.get(widthKey);
+		if (obj[widthKey]) {
+			return obj[widthKey];
+		}
+	} else {
+		return new Promise((resolve, _reject) => {
+			chrome.storage.local.get([widthKey], (result) => {
+				if (result?.width) {
+					return resolve(result.width);
+				}
+				return resolve(defaultWidth);
+			});
+		});
+	}
+	return defaultWidth;
 }
